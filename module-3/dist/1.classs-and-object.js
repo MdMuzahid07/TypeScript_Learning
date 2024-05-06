fbe54028cf893b1a1f4a8655e5f3334f39585033
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-05-06 17:14:36 +0600

    class, and object, and parameter properties

---
+++ module-3/dist/1.classs-and-object.js
@@ -0,0 +1,27 @@
+"use strict";
+{
+    // class Laptop {
+    //     name: string;
+    //     price: number;
+    //     model: string;
+    //     constructor(name: string, price: number, model: string) {
+    //         this.name = name;
+    //         this.price = price;
+    //         this.model = model;
+    //     }
+    //     saySomething() { console.log(`this ${this.name} laptop model is ${this.model}, a great WorkStation Laptop`) }
+    // }
+    // making Laptop class again with ( Parameter Properties )
+    class Laptop {
+        constructor(name, price, model) {
+            this.name = name;
+            this.price = price;
+            this.model = model;
+        }
+        saySomething() {
+            console.log(`this ${this.name} laptop model is ${this.model}, a great WorkStation Laptop`);
+        }
+    }
+    const lenovo = new Laptop("lenovo", 75000, "t450s");
+    lenovo.saySomething();
+}
